0932458f1da7a8e78e47cad9e9d7c702aa2fd60a
adityasneo <128022129+adityasneo@users.noreply.github.com>
2023-04-06 00:16:23 +0530

    Update main.tf

--- ACR/main.tf
+++ ACR/main.tf
@@ -1,16 +1,17 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = var.resource_group_name
 }
 
+
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  name                = var.azurerm_container_registry
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   sku                 = "Premium"
   admin_enabled       = false
   georeplications {
-    location                = "East US"
+    location                = "Central India"
     zone_redundancy_enabled = true
     tags                    = {}
   }
