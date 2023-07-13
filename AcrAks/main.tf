a1e4009a05389524fde2153f8d717615e009e7a9
ADITYA LELLA <128022129+adityasneo@users.noreply.github.com>
2023-07-13 18:04:26 +0530

    Update main.tf

--- AcrAks/main.tf
+++ AcrAks/main.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_version = ">= 1.2"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.11, < 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
 
